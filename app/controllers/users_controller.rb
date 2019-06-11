f654d717e144affbe3fed7c8922f5499fb1c788b
tmetz <tammy@tammymetz.com>
2019-06-11 07:49:52 -0700

    Add to create action in controller to save new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to tasks_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
