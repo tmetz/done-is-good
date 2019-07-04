b4a22c0519bda6719ffe504827475330f93c184e
tmetz <tammy@tammymetz.com>
2019-07-03 17:02:09 -0700

    Add controller action to create new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params(:description))
+
+        if @task.save
+            redirect_to task_path(@task)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:description)
 end
