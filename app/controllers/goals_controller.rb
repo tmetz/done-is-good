258c64d9affe41b3b40f0dae57ac1b1cbdf3068e
tmetz <tammy@tammymetz.com>
2019-07-17 09:25:41 -0700

    Stub out goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,48 @@
 class GoalsController < ApplicationController
+    def new
+        @goal = Goal.new
+    end
+
+    def create
+    end
+
+    def index
+        @goals = Goal.all
+    end
+
+    def show
+        set_goal
+    end
+
+    def edit
+        set_goal
+    end
+
+    def update
+        set_goal
+        if @goal.update(goal_params)
+            redirect_to goal_path(@goal)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        set_goal
+        @goal.destroy
+        redirect_to goals_path
+    end
+
+    private
+
+    def set_goal
+        @goal = Task.find_by(id: params[:id])
+        if !@goal
+            redirect_to goals_path
+        end
+    end
+
+    def goal_params
+        params.require(:goal).permit(:name, :accomplished)
+    end
 end
