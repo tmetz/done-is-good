cc814bab605798aa1d66848c526d8adeb97372c6
tmetz <tammy@tammymetz.com>
2019-07-27 22:26:40 -0700

    Add a custom validation to prevent users from entering too many tasks for an incentive

--- app/models/incentive.rb
+++ app/models/incentive.rb
@@ -1,6 +1,12 @@
 class Incentive < ApplicationRecord
     belongs_to :goal
 
+    validate :max_tasks
+
+    def max_tasks
+        errors.add(:num_tasks, "must be less than or equal to total tasks in goal") if num_tasks > goal.tasks.count
+    end
+
     def percent_complete
         100 * self.goal.tasks.where(done: true).count / self.num_tasks
     end
