0d5e2696887fc638ff9ecd29baa74a865e567f42
tmetz <tammy@tammymetz.com>
2019-08-07 18:46:01 -0700

    Create view helper to display whether a task is accomplished

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,4 +1,11 @@
 module GoalsHelper
 
+    def accomplished_status
+        if @goal.accomplished
+            "Accomplished!"
+        else 
+            "In progress - #{@goal.percent_complete}% done"
+        end 
+    end
     
 end
