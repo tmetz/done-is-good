1f1cbbeeefe10fdc59480f635fb5277dd4e1ffe7
tmetz <tammy@tammymetz.com>
2019-08-07 18:04:38 -0700

    Refactor require_login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
-    include ApplicationHelper
+    
+    helper_method :current_user, :logged_in?, :require_login
+
+    private
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def require_login
+        #return head(:forbidden) unless session.include? :user_id
+        if !logged_in?
+            flash[:error] = "You must be logged in first."
+            redirect_to '/'
+        end
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,3 @@
 module ApplicationHelper
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id])
-    end
-
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def require_login
-        #return head(:forbidden) unless session.include? :user_id
-        if !session.include? :user_id
-            flash[:error] = "You must be logged in first."
-            redirect_to '/'
-        end
-    end
+    
 end
